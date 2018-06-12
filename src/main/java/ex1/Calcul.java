ac98838c6f314ea120bbcc32a61514b41c24aeaa
Mayeul Grivet-Roux <gmayeul@yahoo.fr>
2018-06-12 15:20:01 +0200

    ex1

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,26 @@
+package ex1;
+
+/**
+ * @author gmayeul
+ * Effectue des calculs
+ */
+public class Calcul {
+
+	/**
+	 * @param a
+	 * @param b
+	 * @return somme a + b
+	 */
+	public int additionner(int a, int b){
+		return a + b;
+	}
+	
+	/**
+	 * @param a
+	 * @param b
+	 * @return résultat de a - b
+	 */
+	public int soustraire(int a, int b){
+		return a - b;
+	}
+}
---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,33 @@
+package ex1;
+
+import java.util.Date;
+
+/** 
+ * @author gmayeul
+ * Décrit une entreprise
+ */
+public class Entreprise {
+
+	/** siret (int) */
+	public int siret;
+	
+	/** nom (String) */
+	public String nom;
+	
+	/** adresse (String) */
+	public String adresse;
+	
+	/** dateCreation (Date) */
+	public Date dateCreation;
+	
+	/** CAPITAL_MAX (int) */
+	public static final int CAPITAL_MAX = 3000000;
+	
+	/**
+	 * Affiche le statut de l'entreprise
+	 */
+	public void afficherStatut(){
+		
+	}
+	
+}
